717e0828b9686329a0223e4e38ac979bc2acdef4
s-saferreira <s-saferreira@lwsd.org>
2022-03-02 17:31:25 -0800

    3/2/2022 watched youtube tutorial on event listeners and am now putting them in html file. Still didn't seem to work. I wonder if school blocks pop ups

--- testFileButForJavaScript.js
+++ testFileButForJavaScript.js
@@ -1,11 +1,27 @@
-// I am making a new file to test how JavaScript works with this program
-//let Console = new console;
-//Console.log(takeUserInput());
+// variable to keep track of textBox upon loading
+//let textBox = document.getElementById("textBox");
 
+//load function upon creation.
+window.onload = function () {
+	alert ("Hello");
+	
+	let textBox = document.getElementById("textBox");
+	textBox.addEventListener("click", takeUserInput(), false);
+	
+	// purpose: takeUserInput when run will take a type input and return a string
+	// input: what user types
+	// ouptut: String of what the user typed
+	function takeUserInput() {
+		alert ("Hello");
+		/*if (textBox.text === 'Type here') {
+			textBox.text = 'gaming';
+		} else {
+			textBox.text = 'Type here';
+		}*/	
+	}
+}
 
-let textBox = document.getElementById("textBox");
-textBox.addEventListener("click", takeUserInput);
-
+/*
 // purpose: takeUserInput when run will take a type input and return a string
 // input: what user types
 // ouptut: String of what the user typed
@@ -18,3 +34,4 @@ function takeUserInput() {
 		
 	
 }
+*/
