cd6f97fe5c3a3c797ada9783dddf2183401a5a57
s-saferreira <s-saferreira@lwsd.org>
2022-03-08 08:34:03 -0800

    3/8/2022 8:29 AM added the inputs class which includes dataSender and chat box. The purpose is being able to send text to the server, and also display texts from the server in a chat box

--- inputs.js
+++ inputs.js
@@ -4,21 +4,66 @@
 // purpose: retrieves string or an image from some html class and send to server
 // input: string or image is submit from html
 // output: sends that data to the server
-class dataSender 
+class DataSender 
 {
 	let dSocket;
 	
-	constructor(serverURL) 
+	// for the constructor pass in a WebSocket 
+	constructor(dSocket) 
 	{
-		dSocket = new WebSocket(serverURL);
+		this.dSocket = dSocket;
 	}
 	
-	// purpose:
-	// input:
-	// output:
+	// purpose: sends to server and to what else it needs to be sent to
+	// input: what ever data the player inputted
+	// output: the player 
 	sendToServer(data) 
 	{
 		dSocket.send(data);
 	}
+}
+
+// purpose: retrieve inputted text and output it to the server and the chat box
+// input: the websocket and chat box 
+// output: any user inputs can be sent though websocket and to the chat box
+class ChatDataSender extends DataSender
+{
+	let dSocket;
+	let chatBox;
 	
+	// for the constructor pass in a WebSocket and the chatBox
+	constructor(dSocket, chatBox) 
+	{
+		this.dSocket = dSocket;
+		this.chatBox = chatBox;
+	}
+	
+	// purpose: sends to server and to what else it needs to be sent to
+	// input: what ever data the player inputted
+	// output: the player 
+	sendToServer(data) 
+	{
+		dSocket.send(data);
+	}
+}
+
+//purpose: a chat box that displays messages made by the player, and other players
+//input: create object
+//output: the ability to send data to the chat box and have it display in HTML.
+class ChatBox 
+{
+	let chatHistory;
+	
+	// the constructor creates a list which stores previous messages
+	// NOTE FOR PROGRAMMERS: you probably want to have a system to remove messages
+	// that were sent long enough ago that they no longer display
+	constructor() 
+	{
+		chatHistory = new Array();
+	}
+	
+	retrieveChat(data) 
+	{
+		chatHistory.add(data);
+	}
 }
