bd2fd0214a2e286ec353b575d322dc9279cf963b
s-revertt <s-revertt@lwsd.org>
2022-03-09 20:24:06 -0800

    Switched server backend to Node.js and socket.io

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿// The main server side script
+
+var app = express();
+
+var server = require('http').createServer(app).listen(8080);
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    console.log('connected to client!!!');
+});
