7a637f018358396b399c5614b489e6aa220c9315
s-revertt <s-revertt@lwsd.org>
2022-03-17 11:33:37 -0700

    Fixed MIME type error with Express header

--- client/socket-test.js
+++ client/socket-test.js
@@ -1,8 +1,6 @@
-// const socket = io('http://localhost:8080', {
-// 	extraHeaders: {
-	// 		"Access-Control-Allow-Origin" : "*"
-	// 	}
-	// });
+console.log('reached start of socket-test.js');
+
+import {io} from 'socket.io-client'
 const io = require('socket.io-client');
 const socket = io('http://localhost:8080');
 
--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 ﻿// The main server side script
 
-// var express = require('express');
-// var app = express();
-// var server = require('http').createServer(app);
+var express = require('express');
+express.json("Access-Control-Allow-Origin", "*");
 
-var server = require("live-server");
- 
-var params = {
-    port: 8080, // Set the server port. Defaults to 8080.
-    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-    root: "/public", // Set root directory that's being served. Defaults to cwd.
-    open: false, // When false, it won't load your browser by default.
-    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-    mount: [['/components', './node_modules']], // Mount a directory to a route.
-    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
-};
-server.start(params);
+var mime = require('mime-types'); 
 
-// var server = require('http').createServer(app).listen(8080);
+var app = express();
+app.get('/', function (req, res) { // Sends the basic webpage if GET not speficied
+    res.sendFile(process.cwd() + '/client/index.html');
+});
+app.get('/client/*', function (req, res) { // If a client file is asked for, give it and specify the correct MIME type
+    var mimeType = mime.lookup(req.url);
+    res.setHeader('Content-Type', mimeType);
+});
 
-// var socket = require('socket.io');
-// var io = socket().listen(server);
+var server = require('http').createServer(app).listen(8080);
 
+var socket = require('socket.io');
 
-const io = require('socket.io')(server, {
+const io = socket(server, {
     cors: {
-        origin: 'http://localhost:8080',
+        origin: '*',
         methods: ['GET', 'POST'],
     }
+    
 });
 
 io.sockets.on('connection', function (socket) {
