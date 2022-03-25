37e7e4f10f1d9ececa45ecb407cd408c7516fb23
s-revertt <s-revertt@lwsd.org>
2022-03-24 18:49:10 -0700

    Commented sender & receiver

--- index.js
+++ index.js
@@ -41,7 +41,15 @@ var server = require('http').createServer(app).listen(port, function (error){
 const { Server, Socket, Namespace } = require("socket.io");
 const io = new Server(server);
 
+/**
+ * An object that handles sending a set tagged message to arbitrary client sockets
+ */
 class DataSender {
+    /**
+     * @param {*} tag The tag that is to be sent to the client with the message 
+     * @param {*} defaultRecipients The sockets that the sender defaults to when none are specified
+     * @param  {...any} args The arguments to pass to the client (the data that is sent)
+     */
     constructor(tag, defaultRecipients, ...args)
     {
         this.tag = tag;
@@ -61,7 +69,16 @@ class DataSender {
     }
 }
 
+/**
+ * An object that handles tagged messages received from arbitrary client sockets 
+ */
 class DataReceiver {
+    /**
+     * @param {*} tag The tag that the receiver looks for when detecting messages
+     * @param {*} sockets Sockets to be automatically added to the receiver
+     * @param {*} callback The method that's called when a message is received
+     *  (FIRST PARAMATER MUST TAKE THE SOCKET SENDING THE MESSAGE)
+     */
     constructor(tag, sockets, callback)
     {
         this.tag = tag;
@@ -70,8 +87,11 @@ class DataReceiver {
         this.addSockets(sockets);
     }
 
-    // Converts a namespace into an array of sockets if it isn't already
-    #getSocketArray(sockets)
+    /**
+     * Converts a namespace into an array of sockets if it isn't already
+     * @returns the array version of "sockets"
+     */
+    getSocketArray(sockets)
     {
         if (sockets instanceof Namespace) return sockets.fetchSockets();
         if (sockets instanceof Socket) return [sockets];
@@ -82,10 +102,10 @@ class DataReceiver {
     addSockets(sockets)
     {
         if (sockets == null) return;
-        sockets = this.#getSocketArray(sockets);
-
-        // Only adds it if it isn't already added
+        sockets = this.getSocketArray(sockets);
+        
         sockets.forEach((socket) => {
+            // Only adds it if it isn't already added
             if (!this.socketList.includes(socket))
             {
                 this.socketList.push(socket);
@@ -96,7 +116,11 @@ class DataReceiver {
         })
     }
 
-    // Removes a socket from the receiver (returns true if successful)
+    /**
+     * Stops detecting messages from a given socket
+     * @param socket The socket that is to be removed
+     * @returns true if successful
+     */ 
     remove(socket)
     {
         var index = this.socketList.indexOf(socket);
@@ -105,6 +129,9 @@ class DataReceiver {
         return true;
     }
 
+    /** 
+     * Stops detecting messages sent from all sockets
+     */
     removeAll()
     {
         for (var i = this.socketList.length - 1; i >= 0; i--)
@@ -114,12 +141,14 @@ class DataReceiver {
     }
 }
 
+// For handling passing chat messages between clients:
 chatReceiver = new DataReceiver('chat-message', null, (socket, message) => {
     console.log('Relaying chat message: [' + message + ']');
     socket.rooms.forEach(function (value) {
         socket.to(value).emit('chat-message', message);
     })
 });
+// For when a client requests to join a room:
 roomReqReceiver = new DataReceiver('join-req', null, (socket, message) => {
     console.log('Joining Room: [' + message + ']');
     socket.join(message);
@@ -131,6 +160,7 @@ roomReqReceiver = new DataReceiver('join-req', null, (socket, message) => {
     str2 = str2.substring(22, str2.length - 2)
     socket.emit('rooms-req', str2);
 });
+// For when a client requests to leave a room:
 roomLeaveReceiver = new DataReceiver('leave-rooms', null, (socket, message) => {
     socket.rooms.forEach(function (value) {
         socket.leave(value);
@@ -140,11 +170,12 @@ roomLeaveReceiver = new DataReceiver('leave-rooms', null, (socket, message) => {
 
 // Whenever a new client connects:
 io.on('connection', function (socket) {
-    
+    // Log info and inform other clients that the connection was successful
     console.log('Connected to client at socket id [' + socket.id + ']');
     socket.emit('connection', 'Hello client with id [' + socket.id + ']')
     socket.broadcast.emit('message', 'New user has joined: [' + socket.id + ']');
     
+    // Adds this new client to the global sockets
     chatReceiver.addSockets(socket);
     roomReqReceiver.addSockets(socket);
     roomLeaveReceiver.addSockets(socket);
