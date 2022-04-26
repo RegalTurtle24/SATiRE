a7c73069bbf127eed4f9a928a17f06f1e21a5104
s-revertt <s-revertt@lwsd.org>
2022-04-26 08:10:17 -0700

    Created outline for server side CollabDraw

--- client/game-logic.js
+++ client/game-logic.js
@@ -8,7 +8,7 @@ var game = null;
 // hitting that button
 class GameStartButton {
 
-	constructor(buttonID, emitMessage, getOtherParameters) {
+	constructor(buttonID, emitMessage, gamemode, getOtherParameters) {
 		this.box = document.getElementById(buttonID);
 		// a parameter that could be used to define anything not used in the code. 
 		this.getOtherParameters = getOtherParameters;
@@ -24,7 +24,7 @@ class GameStartButton {
 				other = this.getOtherParameters();
 			}
 
-			socket.emit(emitMessage, joinedRoom, other);
+			socket.emit(emitMessage, gamemode, joinedRoom, other);
 		});
 	}
 }
@@ -81,8 +81,8 @@ function gameLogicInit() {
 		fr.readAsText(file);
 	});
 
-	var startGameButton = new GameStartButton('p5startGame', 'telephone-start', () => {
-		if(policies.length == 0)
+	var startTelephoneButton = new GameStartButton('p5startGame', 'game-start', 'telephone', () => {
+		if (policies.length == 0)
 		{
 			let emptyCharPol = new CharPolicy([""], CharPolicy.REQUIRED, 1, true);
 			policies.push(emptyCharPol);
