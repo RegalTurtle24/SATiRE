6a42bea496a43ecd29c238078423c100c4d9609a
s-revertt <s-revertt@lwsd.org>
2022-05-04 08:01:49 -0700

    Fixed the DrawingPad this. error

--- client/displayAndPad.js
+++ client/displayAndPad.js
@@ -18,9 +18,11 @@ class DrawingPad {
 		// creating eventListener so we can draw
 		this.canvas = document.getElementById(canvasID);
 		this.bounds = this.canvas.getBoundingClientRect();
-		this.canvas.addEventListener("mousedown", this.startDraw);
-		this.canvas.addEventListener("mousemove", this.draw);
-		this.canvas.addEventListener("mouseup", this.cancelDraw);
+		this.canvas.addEventListener("mousedown", (event) => this.startDraw(event));
+		this.canvas.addEventListener("mousemove", (event) => this.draw(event));
+		this.canvas.addEventListener("mouseup", (event) => this.cancelDraw(event));
+		
+		console.log("ok");
 	}
 	
 	addSetting(/*colorSettingID, submittionID, function*/) {
@@ -30,7 +32,8 @@ class DrawingPad {
 	// purpose: starting drawing when click down, get intial cooridinates 
 	startDraw(event) {
 		this.isCurrentlyDrawing = true;
-		this.setPositionAsCurrent(event);
+		this.setCooridinates(event);
+		console.log("draw started");
 	}
 	
 	// purpose:
@@ -39,22 +42,29 @@ class DrawingPad {
 		if (this.isCurrentlyDrawing) {
 			// initialize necessar variables
 			this.context = this.canvas.getContext("2d");
-
-			this.context.beginPath();
+			
+			//set stuff
+			this.context.lineWidth = 20;
+			this.context.lineCap = "round";
+			
+			// -----			
 			this.context.moveTo(this.mouseCooridinatesX, this.mouseCooridinatesY);
-			this.setPositionAsCurrent(event);
+			this.setCooridinates(event);
 			this.context.lineTo(this.mouseCooridinatesX, this.mouseCooridinatesY);
 			this.context.stroke();
+			console.log("drawing");
 		}
 	}
-
-	setPositionAsCurrent(event) {
+	
+	setCooridinates(event) {
 		this.mouseCooridinatesX = event.clientX - this.bounds.left;
 		this.mouseCooridinatesY = event.clientY - this.bounds.top;
-    }
+		console.log(this.mouseCooridinatesX + "" + this.mouseCooridinatesY);
+	}
 	
 	cancelDraw(event) {
 		this.isCurrentlyDrawing = false;
+		console.log("draw cancelled");
 	}
 }
 
